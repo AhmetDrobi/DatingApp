4d47f39fa073712007dbeae46f9cc0abe0018265
AhmadNawar <AhmadNawar@users.noreply.github.com>
2019-06-01 22:52:00 +0300

    inital commit for MySql

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -99,7 +99,7 @@ export class UserService {
     return this.http.post(this.baseUrl + 'users/' + id + '/messages', message);
   }
   deleteMessage(id: number, userId: number) {
-    return this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + id, {})
+    return this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + id, {});
   }
   markAsRead(userId: number, messageId: number) {
     this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + messageId + '/read', {}).subscribe();
